23b53e0a27421e1955870adb3aab76b1497e7bb1
Baran Karakaya <thebarankarakaya@gmail.com>
2024-03-26 19:29:03 +0300

    Update to run from splash screen.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace AWI_Toolkit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainWindow());
+            Application.Run(new SplashScreen());
         }
     }
 }
