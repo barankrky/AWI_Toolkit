9d94ac29616b4bc8ba33104ca971f50907d22bd9
Baran Karakaya <thebarankarakaya@gmail.com>
2024-03-26 19:29:28 +0300

    Update main window.

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -29,26 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.appTopBar = new System.Windows.Forms.Panel();
             this.appClose = new System.Windows.Forms.Button();
             this.appMinimize = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.appSidePanel = new System.Windows.Forms.Panel();
+            this.appNavBar = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCleanup = new System.Windows.Forms.Button();
             this.btnAbout = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.appContainer = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.appTopBar.SuspendLayout();
+            this.appSidePanel.SuspendLayout();
+            this.appNavBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // appTopBar
             // 
-            this.panel1.Controls.Add(this.appClose);
-            this.panel1.Controls.Add(this.appMinimize);
-            this.panel1.Location = new System.Drawing.Point(173, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(728, 30);
-            this.panel1.TabIndex = 0;
+            this.appTopBar.BackColor = System.Drawing.Color.Transparent;
+            this.appTopBar.Controls.Add(this.appClose);
+            this.appTopBar.Controls.Add(this.appMinimize);
+            this.appTopBar.Location = new System.Drawing.Point(0, 0);
+            this.appTopBar.Name = "appTopBar";
+            this.appTopBar.Size = new System.Drawing.Size(901, 30);
+            this.appTopBar.TabIndex = 0;
+            this.appTopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.appTopBar_MouseDown);
             // 
             // appClose
             // 
@@ -56,7 +61,7 @@
             this.appClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.appClose.FlatAppearance.BorderSize = 0;
             this.appClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.appClose.Location = new System.Drawing.Point(691, 0);
+            this.appClose.Location = new System.Drawing.Point(864, 0);
             this.appClose.Name = "appClose";
             this.appClose.Size = new System.Drawing.Size(37, 30);
             this.appClose.TabIndex = 2;
@@ -69,37 +74,61 @@
             this.appMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.appMinimize.FlatAppearance.BorderSize = 0;
             this.appMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.appMinimize.Location = new System.Drawing.Point(657, 0);
+            this.appMinimize.Location = new System.Drawing.Point(830, 0);
             this.appMinimize.Name = "appMinimize";
             this.appMinimize.Size = new System.Drawing.Size(34, 30);
             this.appMinimize.TabIndex = 1;
             this.appMinimize.UseVisualStyleBackColor = true;
             this.appMinimize.Click += new System.EventHandler(this.appMinimize_Click);
             // 
-            // panel2
+            // appSidePanel
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.panel2.Controls.Add(this.btnAbout);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(192, 500);
-            this.panel2.TabIndex = 1;
+            this.appSidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.appSidePanel.Controls.Add(this.appNavBar);
+            this.appSidePanel.Controls.Add(this.pictureBox1);
+            this.appSidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.appSidePanel.Location = new System.Drawing.Point(0, 0);
+            this.appSidePanel.Name = "appSidePanel";
+            this.appSidePanel.Size = new System.Drawing.Size(192, 500);
+            this.appSidePanel.TabIndex = 1;
+            // 
+            // appNavBar
+            // 
+            this.appNavBar.Controls.Add(this.btnCleanup);
+            this.appNavBar.Controls.Add(this.btnAbout);
+            this.appNavBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.appNavBar.Location = new System.Drawing.Point(0, 169);
+            this.appNavBar.Name = "appNavBar";
+            this.appNavBar.Size = new System.Drawing.Size(192, 331);
+            this.appNavBar.TabIndex = 4;
+            // 
+            // btnCleanup
+            // 
+            this.btnCleanup.FlatAppearance.BorderSize = 0;
+            this.btnCleanup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCleanup.Font = new System.Drawing.Font("SF Pro Text", 13F);
+            this.btnCleanup.ForeColor = System.Drawing.Color.White;
+            this.btnCleanup.Location = new System.Drawing.Point(3, 3);
+            this.btnCleanup.Name = "btnCleanup";
+            this.btnCleanup.Size = new System.Drawing.Size(189, 40);
+            this.btnCleanup.TabIndex = 4;
+            this.btnCleanup.Text = "Cleanup";
+            this.btnCleanup.UseVisualStyleBackColor = true;
+            this.btnCleanup.Click += new System.EventHandler(this.btnCleanup_Click);
             // 
             // btnAbout
             // 
-            this.btnAbout.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnAbout.FlatAppearance.BorderSize = 0;
             this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAbout.Font = new System.Drawing.Font("SF Pro Text", 13F);
             this.btnAbout.ForeColor = System.Drawing.Color.White;
-            this.btnAbout.Location = new System.Drawing.Point(0, 169);
+            this.btnAbout.Location = new System.Drawing.Point(3, 49);
             this.btnAbout.Name = "btnAbout";
-            this.btnAbout.Size = new System.Drawing.Size(192, 40);
+            this.btnAbout.Size = new System.Drawing.Size(189, 40);
             this.btnAbout.TabIndex = 3;
             this.btnAbout.Text = "About";
             this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
             // 
             // pictureBox1
             // 
@@ -126,15 +155,16 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(900, 500);
             this.Controls.Add(this.appContainer);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.appSidePanel);
+            this.Controls.Add(this.appTopBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "mainWindow";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AWI Toolkit";
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.appTopBar.ResumeLayout(false);
+            this.appSidePanel.ResumeLayout(false);
+            this.appNavBar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -144,10 +174,12 @@
 
         private System.Windows.Forms.Button appMinimize;
         private System.Windows.Forms.Button appClose;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel appTopBar;
+        private System.Windows.Forms.Panel appSidePanel;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnAbout;
         private System.Windows.Forms.Panel appContainer;
+        private System.Windows.Forms.FlowLayoutPanel appNavBar;
+        private System.Windows.Forms.Button btnCleanup;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AWI_Toolkit.Controls;
@@ -14,16 +15,39 @@ namespace AWI_Toolkit
 {
     public partial class mainWindow : Form
     {
+        Cleanup cleanup = new Cleanup();
+        About about = new About();
+
+        #region Draggable Top Bar
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+        private void showControl(UserControl control)
+        {
+            appContainer.Controls.Clear();
+            appContainer.Controls.Add(control);
+        }
+
 
         public mainWindow()
         {
             InitializeComponent();
-
-            about about = new about();
-            appContainer.Controls.Add(about);
+            showControl(about);
         }
 
         private void appMinimize_Click(object sender, EventArgs e) { WindowState = FormWindowState.Minimized; }
         private void appClose_Click(object sender, EventArgs e) { Application.Exit(); }
+        private void btnCleanup_Click(object sender, EventArgs e) { showControl(cleanup); }
+        private void btnAbout_Click(object sender, EventArgs e) { showControl(about); }
+
+        private void appTopBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
     }
 }
