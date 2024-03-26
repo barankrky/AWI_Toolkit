6e94edffc28ed35aab3093ee22c6f4682707e465
Baran Karakaya <thebarankarakaya@gmail.com>
2024-03-26 19:29:17 +0300

    Add splash screen.

---
+++ SplashScreen.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace AWI_Toolkit
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 175);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::AWI_Toolkit.Properties.Resources.awi;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.statusLabel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 175);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 25);
+            this.panel2.TabIndex = 1;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("SF Pro Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(0, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.statusLabel.Size = new System.Drawing.Size(400, 25);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Loading...";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AWI Toolkit";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
---
+++ SplashScreen.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.IO;
+using AWI_Toolkit.Modules;
+using AWI_Toolkit;
+
+namespace AWI_Toolkit
+{
+    public partial class SplashScreen : Form
+    {
+        ShellExecutioner executer = new ShellExecutioner();
+
+        private async void setStatus(string statusText, int waitTime = 1) { 
+            statusLabel.Text = statusText;
+            await Task.Delay(waitTime);
+        }
+
+        private void startMainWindow()
+        {
+            Hide();
+            mainWindow main = new mainWindow();
+            main.Show();
+        }
+
+        public bool IsFontInstalled(string ContentFontName)
+        {
+            return File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), ContentFontName.ToUpper()));
+        }
+
+        private async void checkResources()
+        {
+            setStatus("Checking resources...");
+            await Task.Delay(1000);
+            // Check the font file is installed.
+
+            if (IsFontInstalled("sf.ttf"))
+            {
+                setStatus("Font already installed.");
+            }
+            else
+            {
+                setStatus("Font not found. Attempting to install...");
+                executer.InstallFontFile();
+            }
+            
+        }
+
+        private async void Initialize()
+        {
+            checkResources();
+            await Task.Delay(3000);
+            startMainWindow();
+        }
+
+        
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private async void SplashScreen_Load(object sender, EventArgs e)
+        {
+            setStatus("Initializing...");
+            Initialize();
+
+        }
+    }
+}
